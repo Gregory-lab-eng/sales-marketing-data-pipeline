26cdac0eb2d2266f829b85e5023b5ef7573dec70
grzegorz.strachow@gmail.com <grzegorz.strachow@gmail.com>
2025-10-20 11:02:00 +0000

    added ingestion function, code refactor

--- sales-pipeline/transformations/ingest_to_bronze.py
+++ sales-pipeline/transformations/ingest_to_bronze.py
@@ -2,12 +2,12 @@ from pyspark import pipelines as dp
 from pyspark.sql.functions import col
 from utilities import utils
 
+catalog = "bronze"
+schema = "kaggle"
+files_storage = "abfss://landing@salesmarketingstorage1.dfs.core.windows.net/"
 
-@dp.table(
-    name="bronze.kaggle.dp_customers",
-    comment="Raw customer data ingested from CSV via Auto Loader"
-)
-def bronze_customers():
+
+def ingest_csv_from_landing(folder: str):
     return (
         spark.readStream
         .format("cloudFiles")
@@ -15,127 +15,78 @@ def bronze_customers():
         .option("header", "true")
         .option("quote", "\"")
         .option("mode", "PERMISSIVE")
-        .load("abfss://landing@salesmarketingstorage1.dfs.core.windows.net/customers/")
+        .load(f"{files_storage}/{folder}/")
     )
 
 
 @dp.table(
-    name="bronze.kaggle.dp_geolocation",
+    name=f"{catalog}.{schema}.dp_customers",
+    comment="Raw customer data ingested from CSV via Auto Loader"
+)
+def bronze_customers():
+    return (ingest_csv_from_landing("customers")) 
+
+
+@dp.table(
+    name=f"{catalog}.{schema}.dp_geolocation",
     comment="Raw geolocation data ingested from CSV via Auto Loader"
 )
 def bronze_geolocation():
-    return (
-    spark.readStream 
-    .format("cloudFiles")
-    .option("cloudFiles.format", "csv")
-    .option("header", "true")
-    .option("quote", "\"")
-    .option("mode", "PERMISSIVE")
-    .load("abfss://landing@salesmarketingstorage1.dfs.core.windows.net/geolocations/")
-    )
+    return (ingest_csv_from_landing("geolocations"))
 
 
 @dp.table(
-    name="bronze.kaggle.dp_order_items",
+    name=f"{catalog}.{schema}.dp_order_items",
     comment="Raw order_items data ingested from CSV via Auto Loader"
 )
 def order_items():
-    return (
-    spark.readStream 
-    .format("cloudFiles").option("cloudFiles.format", "csv")
-    .option("header", "true")
-    .option("quote", "\"")
-    .option("mode", "PERMISSIVE")
-    .load("abfss://landing@salesmarketingstorage1.dfs.core.windows.net/order_items/")
-    )
+    return (ingest_csv_from_landing("order_items"))
 
 
 @dp.table(
-    name="bronze.kaggle.dp_payments",
+    name=f"{catalog}.{schema}.dp_payments",
     comment="Raw payments data ingested from CSV via Auto Loader"
 )
 def payments():
-    return (
-    spark.readStream 
-    .format("cloudFiles").option("cloudFiles.format", "csv")
-    .option("header", "true")
-    .option("quote", "\"")
-    .option("mode", "PERMISSIVE")
-    .load("abfss://landing@salesmarketingstorage1.dfs.core.windows.net/order_payments/")
-    )
+    return (ingest_csv_from_landing("order_payments"))
 
 
 @dp.table(
-    name="bronze.kaggle.dp_order_reviews",
+    name=f"{catalog}.{schema}.dp_order_reviews",
     comment="Raw order_reviews data ingested from CSV via Auto Loader"
 )
 def order_reviews():
-    return(
-    spark.readStream 
-    .format("cloudFiles").option("cloudFiles.format", "csv")
-    .option("header", "true")
-    .option("quote", "\"")
-    .option("mode", "PERMISSIVE")
-    .load("abfss://landing@salesmarketingstorage1.dfs.core.windows.net/order_reviews/")
-    )
+    return (ingest_csv_from_landing("order_reviews"))
 
 
 @dp.table(
-    name="bronze.kaggle.dp_orders",
+    name=f"{catalog}.{schema}.dp_orders",
     comment="Raw orders data ingested from CSV via Auto Loader"
 )
 def orders():
-    return (
-    spark.readStream 
-    .format("cloudFiles").option("cloudFiles.format", "csv")
-    .option("header", "true")
-    .option("quote", "\"")
-    .option("mode", "PERMISSIVE")
-    .load("abfss://landing@salesmarketingstorage1.dfs.core.windows.net/orders/")
-    )
+    return (ingest_csv_from_landing("orders"))
 
 @dp.table(
-    name="bronze.kaggle.dp_products",
+    name=f"{catalog}.{schema}.dp_products",
     comment="Raw products data ingested from CSV via Auto Loader"
 )
 def products():
-    return (
-    spark.readStream 
-    .format("cloudFiles").option("cloudFiles.format", "csv")
-    .option("header", "true")
-    .option("quote", "\"")
-    .option("mode", "PERMISSIVE")
-    .load("abfss://landing@salesmarketingstorage1.dfs.core.windows.net/products/")
-)
+    return (ingest_csv_from_landing("products"))
     
 
 @dp.table(
-    name="bronze.kaggle.dp_sellers",
+    name=f"{catalog}.{schema}.dp_sellers",
     comment="Raw sellers data ingested from CSV via Auto Loader"
 )
 def sellers():
-    return (
-    spark.readStream 
-    .format("cloudFiles").option("cloudFiles.format", "csv")
-    .option("header", "true")
-    .option("quote", "\"")
-    .option("mode", "PERMISSIVE")
-    .load("abfss://landing@salesmarketingstorage1.dfs.core.windows.net/sellers/")
-    )
+    return (ingest_csv_from_landing("sellers"))
 
 
 @dp.table(
-    name="bronze.kaggle.dp_product_category_name_translation",
+    name=f"{catalog}.{schema}.dp_product_category_name_translation",
     comment="Raw product_category_name_translation data ingested from CSV via Auto Loader"
 )
 def product_category_name_translation():
-    return (
-    spark.readStream 
-    .format("cloudFiles").option("cloudFiles.format", "csv")
-    .option("header", "true")
-    .option("quote", "\"")
-    .option("mode", "PERMISSIVE")
-    .load("abfss://landing@salesmarketingstorage1.dfs.core.windows.net/category_name/")
-    )
+    return (ingest_csv_from_landing("category_name"))
 
 
